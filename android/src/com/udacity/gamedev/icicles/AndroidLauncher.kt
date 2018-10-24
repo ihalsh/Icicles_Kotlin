8730ca560f7c1558ab91a52566dba0908af5b670
Igor <igor.sheremet@gmail.com>
2018-10-24 13:18:34 +0300

    Initial commit

---
+++ android/src/com/udacity/gamedev/icicles/AndroidLauncher.kt
@@ -0,0 +1,15 @@
+package com.udacity.gamedev.icicles
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.udacity.gamedev.icicles.IciclesGame
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(IciclesGame(), config)
+    }
+}
---
+++ desktop/src/com/udacity/gamedev/icicles/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.udacity.gamedev.icicles.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.udacity.gamedev.icicles.IciclesGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(IciclesGame(), config)
+    }
+}
